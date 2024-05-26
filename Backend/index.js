d92cc7a63061060e653e767ccf8d5de949e1baf2
Priyesh <priyeshchaudhari35@gmail.com>
2024-05-26 23:53:48 +0530

    updating my backend commit

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 
-app.post('/todo' , function(req,res){
+app.post('/todo' , async function(req,res){
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
 
@@ -20,13 +20,25 @@ app.post('/todo' , function(req,res){
         return;
     }
     // put it in mongodb
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo created successfully!!!"
+    })
 })
 
-app.get('/todos' , function(req,res){
-    
+app.get('/todos' , async function(req,res){
+    const todos =await todo.find({})
+    res.json({
+        todos   
+    })
 })
 
-app.put('/completed' , function(req,res){
+app.put('/completed' , async function(req,res){
     const updatePayload = req.body;
     const parsedPayload = createTodo.safeParse(updatePayload);
 
@@ -36,5 +48,13 @@ app.put('/completed' , function(req,res){
         })
         return;
     }
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed:true
+    })
 
+    res.json({
+        msg: "Todo marked as completed"
+    })
 })
